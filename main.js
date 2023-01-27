c05d08e3c2f70a064bad1afaa864ebf5724d45e1
Femi-Laizer <59768131+FemiLaizer@users.noreply.github.com>
2023-01-27 23:56:28 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-console.log("connected");
-console.log("connected");
-console.log("connected");
+const container = document.querySelector('.container');
+const firstDisplay = container.querySelector('.first-display');
+const menuDisplay = container.querySelector('.menu-display');
+
+const displayMenu = (e) => {
+    // console.log(e.currentTarget)
+    if (e.currentTarget.classList.contains("hidden")) {
+        e.currentTarget.classList.remove("hidden");
+        menuDisplay.style.display = "none"
+    } else {
+        e.currentTarget.classList.add('hidden')
+        menuDisplay.style.display = "flex"
+    }
+}
+firstDisplay.addEventListener('click', displayMenu)
